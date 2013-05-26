f3517ab56caa2f4465a94e2b221fb992cdcda74b
ruscoder <rusmiligamer@mail.ru>
2013-05-26 22:28:36 +0800

    Added docstring to sprites module. Generet autodoc by Epydoc. doc/ dir

--- sprites.py
+++ sprites.py
@@ -9,7 +9,17 @@ from constant import *
 
 
 class Wall(pygame.sprite.Sprite):
+    """
+    Wall Class
+    Static sprite, rendered to static background
+    """
+
     def __init__(self, x, y):
+        """
+        Constructor initializes new rect in
+        x and y coordinates.
+        Also generates mask (square)
+        """
         super(Wall, self).__init__()
         self.rect = pygame.Rect(
             OFFSET + x, OFFSET + y,
@@ -22,14 +32,18 @@ class Wall(pygame.sprite.Sprite):
         self.mask.fill()
 
     def render(self, background):
+        """This method render sprite to static background"""
         pygame.draw.rect(background, COLOR_WALL, self.rect)
 
 
 class Robot(pygame.sprite.Sprite):
-    """ Robot sprite class """
+    """Robot sprite class"""
 
     def __init__(self, screen, walls):
-        """ init """
+        """
+        Constructor initializes new sprite, load image.
+        Create mask from image, defines startup vars.
+        """
         super(Robot, self).__init__()
         self.walls = walls
         self.image = pygame.image.load("irobot.png").convert_alpha()
@@ -51,17 +65,20 @@ class Robot(pygame.sprite.Sprite):
         )
 
     def move_forward(self):
-        """ move forward"""
+        """This method changes coords by speed"""
         self.rect.move_ip(self.speedx, self.speedy)
 
     def render(self, screen):
+        """"This method render sprite to screen"""
         screen.blit(self.image, self.rect)
 
     def compute_speed(self):
+        """Compute speed X and Y for this degree"""
         self.speedx = math.trunc(math.cos(self.deg * math.pi / 180) * 5)
         self.speedy = -math.trunc(math.sin(self.deg * math.pi / 180) * 5)
 
     def rotate(self, deg):
+        """Rotate image for this degree"""
         self.deg += deg
         # Save center
         old_center = self.rect.center
@@ -73,6 +90,7 @@ class Robot(pygame.sprite.Sprite):
         self.rect.center = old_center
 
     def can_move_forward(self):
+        """This method returns True if sprite can move forward"""
         old_rect = self.rect
         self.rect = self.rect.move(self.speedx, self.speedy)
         can = pygame.sprite.spritecollideany(
